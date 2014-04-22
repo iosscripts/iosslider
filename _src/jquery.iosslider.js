cfc0fa94156b73b585a1231da5f88c0d2d5c2adc
Marc Whitbread <marcwhitbread@gmail.com>
2014-04-22 10:25:32 -0400

    v1.3.42
    Prevent keyboardControls: true from navigating when using input. Ref
    issue #333

--- _src/jquery.iosslider.js
+++ _src/jquery.iosslider.js
@@ -9,7 +9,7 @@
  * 
  * Copyright (c) 2013 Marc Whitbread
  * 
- * Version: v1.3.41 (04/16/2014)
+ * Version: v1.3.42 (04/22/2014)
  * Minimum requirements: jQuery v1.4+
  *
  * Advanced requirements:
